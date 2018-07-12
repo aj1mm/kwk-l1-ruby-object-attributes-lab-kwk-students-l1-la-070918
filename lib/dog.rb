5ac6e9318570433a8826d8e72ade9b80ee8d5b91
aj1mm <avaijimmerson@gmail.com>
2018-07-12 22:21:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,8 @@
 # dog.rb
 class Dog 
-  def initialize(name,breed)
-    attr_accessor :name
+  attr_accessor :name
     attr_accessor :breed
-    def initialize(name,breed)
+  def initialize(name,breed)
     @name = name
     @breed = breed
     
